13987fdec3caf80ed8499a1cce59e2099f65af9a
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-10-02 17:23:36 -0400

    Added recursive fibonacci function.
    The function uses recursion to calculate an array that generates n fibonacci numbers.

--- recursion.js
+++ recursion.js
@@ -17,3 +17,21 @@ function fibs(n) {
   }
   return sequence;
 }
+
+function fibsRec(n, sequence = [0, 1]) {
+  if (n === 2) {
+    return sequence;
+  }
+  if (n < 2) {
+    if (n === 0) {
+      return [];
+    } else if (n === 1) {
+      return [0];
+    } else {
+      console.error("Only positive numbers please");
+      return;
+    }
+  }
+  sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
+  return fibsRec(n - 1, sequence);
+}
