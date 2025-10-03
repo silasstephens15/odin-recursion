c413394a8fd232e02cbaaeb6bbf817a179aeed72
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-10-03 15:20:39 -0400

    Added merge sort function.
    This is an efficient method to sort a large array.

--- recursion.js
+++ recursion.js
@@ -35,3 +35,27 @@ function fibsRec(n, sequence = [0, 1]) {
   sequence.push(sequence[sequence.length - 1] + sequence[sequence.length - 2]);
   return fibsRec(n - 1, sequence);
 }
+
+function mergeSort(arr, sorted = [], compare1 = [], compare2 = []) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  compare1 = mergeSort(arr.slice(0, Math.round(arr.length / 2)));
+  compare2 = mergeSort(arr.slice(Math.round(arr.length / 2), arr.length));
+  while (compare1.length !== 0 && compare2.length !== 0) {
+    if (compare1[0] < compare2[0]) {
+      sorted.push(compare1[0]);
+      compare1.shift();
+    } else {
+      sorted.push(compare2[0]);
+      compare2.shift();
+    }
+  }
+  if (compare1.length !== 0) {
+    sorted.push(...compare1);
+  }
+  if (compare2.length !== 0) {
+    sorted.push(...compare2);
+  }
+  return sorted;
+}
